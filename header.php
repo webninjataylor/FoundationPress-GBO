1f480d74b2ff348e47581ac3a39b1f03b4816324
webninjataylor <webninjataylor@gmail.com>
2018-08-24 09:47:08 -0400

    Added fonts from design doc.

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link href="https://fonts.googleapis.com/css?family=Abel|Montserrat:100,400" rel="stylesheet" />
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
