fc325f2a07942b7e6c2317833a39923ddb5e0016
webninjataylor <webninjataylor@gmail.com>
2018-08-31 20:43:53 -0400

    Header styling.

--- header.php
+++ header.php
@@ -59,6 +59,7 @@
 			</div>
 			<div class="top-bar-right">
 				<?php foundationpress_top_bar_r(); ?>
+				<div class="menu-util-sticky vertical"><?php foundationpress_top_bar_util(); ?></div>
 				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
 					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
 				<?php endif; ?>
