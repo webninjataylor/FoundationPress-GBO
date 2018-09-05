c26515e00cd57d106486d54b42292851f0ee3fba
webninjataylor <webninjataylor@gmail.com>
2018-09-05 00:10:38 -0400

    Home page cards setup.

--- page-templates/front.php
+++ page-templates/front.php
@@ -57,42 +57,33 @@ get_header(); ?>
 </div>
 
 
-<section class="benefits">
-	<header>
-		<h2>Build Foundation based sites, powered by WordPress</h2>
-		<h4>Foundation is the professional choice for designers, developers and teams. <br /> WordPress is by far, <a href="http://trends.builtwith.com/cms">the world's most popular CMS</a> (currently powering 38% of the web).</h4>
-	</header>
 
-	<div class="semantic">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/demo/semantic.svg" alt="semantic">
-		<h3>Semantic</h3>
-		<p>Everything is semantic. You can have the cleanest markup without sacrificing the utility and speed of Foundation.</p>
+
+
+<!-- GBO: TEST CARDS -->
+<div class="cards-container">
+	<?php $catquery = new WP_Query( 'cat=9&posts_per_page=6' ); ?>
+		<?php while($catquery->have_posts()) : $catquery->the_post(); ?>
+			<h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+			<div><?php the_content(); ?></div>
+		<?php endwhile; ?>
+	<?php wp_reset_postdata(); ?>
+
+	<div class="card">
+	    <img src="https://placeimg.com/300/200/nature">
+	    <div class="card-content">
+	        <h4>Menus</h4>
+	        <p>Cards play nicely with menus too! Give them a try.</p>
+	        <ul class="menu simple">
+	            <li><a href="#">One</a></li>
+	            <li><a href="#">Two</a></li>
+	            <li><a href="#">Three</a></li>
+	        </ul>
+	    </div>
 	</div>
+</div>
 
-	<div class="responsive">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/demo/responsive.svg" alt="responsive">
-		<h3>Responsive</h3>
-		<p>You can build for small devices first. Then, as devices get larger and larger, layer in more complexity for a complete responsive design.</p>
 
-	</div>
 
-	<div class="customizable">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/demo/customizable.svg" alt="customizable">
-		<h3>Customizable</h3>
-		<p>You can customize your build to include or remove certain elements, as well as define the size of columns, colors, font size and more.</p>
-
-	</div>
-
-	<div class="professional">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/demo/professional.svg" alt="professional">
-		<h3>Professional</h3>
-		<p>Millions of designers and developers depend on Foundation. We have business support, training and consulting to help grow your product or service.</p>
-	</div>
-
-	<div class="why-foundation">
-		<a href="/kitchen-sink">See what's in Foundation out of the box →</a>
-	</div>
-
-</section>
 
 <?php get_footer();
